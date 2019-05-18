1af08cd9c9227a463d30e34552aac4701a46cc1d
haniejuraimi <haniealia08@gmail.com>
2019-05-18 11:58:19 +0800

    Initial commit

---
+++ sksjfjgkgb.js
@@ -0,0 +1,14 @@
+const iDontKnow = [10, 12, 14];
+let sum = 0;
+
+for (let value of iDontKnow); {
+  console.log(value);
+  sum = sum + value;
+}
+
+let result = sum / iDontKnow.length
+console.log(result)
+
+
+
+
